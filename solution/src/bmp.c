ef176b462501c8f2326185ac9c79346efa1987c7
plokiju <plokiju@niuitmo.ru>
2022-12-25 21:42:35 +0300

    Исправление комментариев

--- solution/src/bmp.c
+++ solution/src/bmp.c
@@ -3,7 +3,7 @@
 //
 #include "../include/bmp.h"
 
-static uint32_t padding_calculate( const uint32_t width ){
+uint32_t padding_calculate( const uint32_t width ){
 
     return ( 4 - ( width * 3 ) % 4 ) % 4;
 
@@ -15,7 +15,10 @@ enum read_status from_bmp( FILE* in, struct image* img ){
 
     uint32_t padding, width, height = 0;
 
-    fread( &header, sizeof( struct bmp_header ), 1, in );
+    enum read_status now_read_status = fread( &header, sizeof( struct bmp_header ), 1, in );
+    if ( now_read_status ){
+        return READ_INVALID_HEADER;
+    }
 
     width = header.biWidth;
     height = header.biHeight;
@@ -27,8 +30,8 @@ enum read_status from_bmp( FILE* in, struct image* img ){
     }
 
     for ( uint32_t i = 0; i < height; i = i + 1 ){
-
-        if ( !( fread( ( img->data + i * width ), sizeof( struct pixel ) * width, 1, in ))){
+        enum read_status now_bits_read_status = fread( ( img->data + i * width ), sizeof( struct pixel ) * width, 1, in );
+        if ( now_bits_read_status ){
             return READ_INVALID_BITS;
         }
 
@@ -41,19 +44,29 @@ enum read_status from_bmp( FILE* in, struct image* img ){
 
 enum write_status to_bmp( FILE* out, const struct image* img ){
 
-    const char pad_byte[3] = { 0, 0, 0 };
+    const char pad_byte = { 0, 0, 0 };
 
     struct bmp_header new_header = new_bmp_header( img->width, img->height );
 
     uint32_t padding = padding_calculate(img->width );
 
-    fwrite(&new_header, sizeof( struct bmp_header ), 1, out );
+    enum write_status now_write_status = fwrite(&new_header, sizeof( struct bmp_header ), 1, out );
+    if ( now_write_status ){
+        return WRITE_ERROR;
+    }
 
     for ( uint32_t j = 0; j < img->height; j = j + 1 ){
 
-        fwrite( ( img->data + j * img->width ), sizeof( struct pixel ) * img->width, 1, out );
+        enum write_status now_bits_write_status = fwrite( ( img->data + j * img->width ), sizeof( struct pixel ) * img->width, 1, out );
+        if ( now_bits_write_status ){
+            return WRITE_ERROR;
+        }
 
-        fwrite( &pad_byte, 1, padding, out );
+        enum write_status now_pad_byte_write_status = fwrite( &pad_byte, 1, padding, out );
+        if ( now_pad_byte_write_status ){
+            return WRITE_ERROR;
+        }
+
     }
 
     return WRITE_OK;
@@ -63,7 +76,7 @@ enum write_status to_bmp( FILE* out, const struct image* img ){
 struct bmp_header new_bmp_header(uint32_t width, uint32_t height){
 
     struct bmp_header new_header;
-    uint32_t padding = padding_calculate(width );
+    uint32_t padding = padding_calculate( width );
 
     uint32_t size_image = 3 * height * width + width * padding;
     uint32_t file_size = size_image + sizeof ( struct bmp_header );
